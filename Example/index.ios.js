decda3eb42612c55b0e7b475b77ba518aa9ec194
Pavlo Aksonov <aksonov@gmail.com>
2015-09-10 08:36:29 +0200

    modify Example to demonstrate background

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -19,18 +19,18 @@ var Example = React.createClass({
     render: function() {
         return (
             <View style={{flex:1}}>
-                <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'black'}}/>
-            <Router>
-                <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} navBar={NavBarModal} />
-                <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} navBar={NavBar} />
-                <Schema name="withoutAnimation" navBar={NavBar} />
+                <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'#F5FCFF'}}/>
+                <Router>
+                    <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} navBar={NavBarModal} />
+                    <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} navBar={NavBar} />
+                    <Schema name="withoutAnimation" navBar={NavBar} />
 
-                <Route name="launch" component={Launch} initial={true} hideNavBar={true} title="Launch"/>
-                <Route name="register" component={Register} title="Register" />
-                <Route name="login" component={Login} schema="modal"/>
-                <Route name="register2" component={Register} schema="withoutAnimation" />
-                <Route name="error" component={Error} schema="popup"/>
-            </Router>
+                    <Route name="launch" component={Launch} initial={true} hideNavBar={true} title="Launch"/>
+                    <Route name="register" component={Register} title="Register" />
+                    <Route name="login" component={Login} schema="modal"/>
+                    <Route name="register2" component={Register} schema="withoutAnimation" />
+                    <Route name="error" component={Error} schema="popup"/>
+                </Router>
 
             </View>
         );
