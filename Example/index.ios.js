701416890d6915d3480a3c7caf5f1b25f11dbf44
Pavlo Aksonov <aksonov@Pavlos-MacBook-Pro.local>
2015-09-25 15:01:47 +0200

    use ES6 style for demo

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -15,26 +15,26 @@ var {Router, Route, Actions, Animations, Schema} = require('react-native-router-
 var {NavBar, NavBarModal} = require('./components/NavBar');
 var Error = require('./components/Error');
 
-var Example = React.createClass({
-    render: function() {
+class Example extends React.Component {
+    render() {
         return (
             <View style={{flex:1}}>
                 <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'#F5FCFF'}}/>
                 <Router>
-                    <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} navBar={NavBarModal} />
-                    <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} navBar={NavBar} />
-                    <Schema name="withoutAnimation" navBar={NavBar} />
+                    <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} navBar={NavBarModal}/>
+                    <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} navBar={NavBar}/>
+                    <Schema name="withoutAnimation" navBar={NavBar}/>
 
                     <Route name="launch" component={Launch} initial={true} hideNavBar={true} title="Launch"/>
-                    <Route name="register" component={Register} title="Register" />
+                    <Route name="register" component={Register} title="Register"/>
                     <Route name="login" component={Login} schema="modal"/>
-                    <Route name="register2" component={Register} schema="withoutAnimation" />
+                    <Route name="register2" component={Register} schema="withoutAnimation"/>
                     <Route name="error" component={Error} schema="popup"/>
                 </Router>
 
             </View>
         );
     }
-});
+}
 
 AppRegistry.registerComponent('Example', () => Example);
