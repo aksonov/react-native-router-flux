bec31e78efd5f9ca3c72008e03778ddf0f074a98
Qiao Liang <qiaoliang89@gmail.com>
2015-11-02 13:01:57 +0800

    fix iOS demo[bundle url & packager failed with npm start]

--- Example/iOS/AppDelegate.m
+++ Example/iOS/AppDelegate.m
@@ -31,7 +31,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
 
   /**
    * OPTION 2
@@ -49,7 +49,7 @@
                                                       moduleName:@"Example"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [[UIViewController alloc] init];
   rootViewController.view = rootView;
