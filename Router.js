29b9b61eaf0227b6d298c4d36289c26acbf7ee6c
Pavlo Aksonov <aksonov@MacBook-Pro-Pavlo.local>
2015-12-30 09:21:50 +0100

    new 2.0 (alpha) version

---
+++ Router.js
@@ -0,0 +1,195 @@
+/**
+ * Copyright (c) 2015-present, Pavel Aksonov
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import Route from './Route';
+import Actions from './Actions';
+
+export class RouterDelegate {
+    onPush(name:string, props:{ [key: string]: any}):boolean {
+        return true;
+    }
+    onPop(name:string, props:{ [key: string]: any}): boolean {
+        return true;
+    }
+    onReplace(name:string, props:{ [key: string]: any}):boolean {
+        return true;
+    }
+    onSwitch(name:string, props:{ [key: string]: any}):boolean {
+        return true;
+    }
+}
+
+export default class Router {
+    name:string;
+    routes: {[key: string]: Route };
+    schemas: {[key: string]: {[key:string]:any } };
+    props: { [key: string]: any};
+    parentRoute: ?Route;
+    _stack: Array<string>;
+    delegate:RouterDelegate;
+
+    set stack(stack:Array<string>) {
+        if (!stack || !stack.length) {
+            throw new Error("Cannot be set to empty stack");
+        }
+        this._stack = stack;
+    }
+
+    get stack():Array<string> {
+        return this._stack;
+    }
+
+    get currentRoute():Route {
+        return this.routes[this._stack[this._stack.length-1]];
+    }
+
+    get previousRoute():Route {
+        if (this._stack.length > 1){
+            return this.routes[this._stack[this._stack.length-2]];
+        } else {
+            return null;
+        }
+    }
+
+    constructor(routes: Array<{ [key: string]: any}>,
+                schemas:Array<{ [key: string]: any}> = [],
+                stack:Array<string> = null, props:{ [key: string]: any} = {}){
+        this.schemas = {};
+        this.routes = {};
+        this.delegate = new RouterDelegate();
+        if (!routes || !routes.length){
+            throw new Error("routes is not defined");
+        }
+        this.props = props;
+        this.name = props && props.name;
+        this.parentRoute = props && props.route;
+        if (this.parentRoute){
+            if (this.parentRoute.parent){
+                // copy parent schemas
+                Object.keys(this.parentRoute.parent.schemas).forEach(el=>
+                    this._addSchema(this.parentRoute.parent.schemas[el].name, this.parentRoute.parent.schemas[el]));
+            }
+            this.parentRoute.childRouter = this;
+        }
+
+
+        schemas.forEach(el=>this._addSchema(el.name, el));
+        routes.forEach(el=>this._addRoute(el.name, el));
+
+        // select first one as initial
+        if (!stack || !stack.length){
+            stack = [routes[0].name];
+        }
+        this.stack = stack;
+        // add actions
+        this._addActions();
+    }
+
+    _addSchema(name: string, props:{ [key: string]: any}){
+        if (!name){
+            throw new Error("Schema name is not defined");
+        }
+        if (this.schemas[name]){
+            throw new Error("Schema="+name+" is not unique!");
+        }
+        this.schemas[name] = props;
+    }
+
+    _addRoute(name: string, props:{ [key: string]: any}){
+        if (!name){
+            throw new Error("Route name is not defined");
+        }
+        const schemaName: string = props.schema || 'default';
+        const schema = this.schemas[schemaName] || {};
+        // pass router data to inner routes
+        const {children, data, ...routerProps}  = this.props;
+        const routeProps = Object.assign({}, data, schema, props);
+
+        if (this.routes[name]){
+            throw new Error("Route="+name+" is not unique!");
+        }
+
+        this.routes[name] = new Route(routeProps, this);
+
+    }
+
+    _addActions(){
+        if (!Actions.currentRouter){
+            Actions.currentRouter = this;
+        }
+        Object.keys(this.routes).forEach(name=>{
+            if (!Actions[name]){
+                Actions[name] = function(data){
+                    return Actions.route(name, data);
+                }
+            }
+        });
+    }
+
+    route(name: string, props:{ [key: string]: any} = {}){
+        if (!this.routes[name]){
+            throw new Error("No route is defined for name="+name);
+        }
+        const action = this.routes[name].type === "switch" ? "jump": this.routes[name].type;
+        if (!action){
+            throw new Error("No type is defined for name="+name);
+        }
+        if (!this["_"+action]){
+            throw new Error("No type="+action+" is supported");
+        }
+
+        const handler = "on"+capitalizeFirstLetter(action);
+        if (this.delegate[handler]) {
+            console.log("Run handler "+handler);
+            const res:boolean = this.delegate[handler](this.routes[name], props);
+            if (!res) {
+                console.log("Ignore push, handler returns false");
+                return false;
+            }
+        } else {
+            throw new Error("No handler "+handler+" for route="+name);
+        }
+        this["_"+action](name, props);
+        return true;
+    }
+
+    _push(name:string, props:{ [key: string]: any} ){
+        this._stack.push(name);
+    }
+
+    _replace(name:string, props:{ [key: string]: any} ) {
+        this._stack[this._stack.length - 1] = name;
+    }
+
+    _jump(name:string, props:{ [key: string]: any} ) {
+        if (this._stack.indexOf(name) != -1){
+            const pos = this._stack.indexOf(name);
+            // swap latest with found element (because currentRoute always points to latest)
+            this._stack[pos] = this._stack[this._stack.length - 1];
+            this._stack[this._stack.length - 1] = name;
+        } else {
+            this._stack.push(name);
+        }
+    }
+
+    pop(num: number = 1){
+        if (this._stack.length <= num){
+            throw new Error("Cannot pop(), stack=["+this._stack+"]");
+        }
+        if (this.delegate.onPop && this.delegate.onPop(num)){
+            const routes = this._stack.splice(-num, num);
+            return true;
+        }
+        return false;
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
---
+++ TabBar.js
@@ -0,0 +1,68 @@
+import React from 'react-native'
+const {View, InteractionManager} = React;
+import Tabs from 'react-native-tabs';
+
+import Actions from './Actions';
+export default class TabBar extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    onSelect(el){
+        if (!Actions[el.props.name]){
+            throw new Error("No action is defined for name="+el.props.name+" actions:"+JSON.stringify(Object.keys(Actions)));
+        }
+        Actions[el.props.name](el.props);
+        InteractionManager.runAfterInteractions(() =>
+            this.setState({hideTabBar: el.props.hideTabBar}));
+        return {selected: true};
+    }
+    getChildrenState(selectedRoute){
+        var self = this;
+        let selected = false;
+        var children = [];
+        React.Children.forEach(this.props.children, function(el, index){
+            const schema = self.props.schemas && el.props.schema && self.props.schemas[el.props.schema] ? self.props.schemas[el.props.schema] : {};
+            let props = {...schema, ...el.props};
+            if (!el.props.name)
+                console.error("No name is defined for element");
+            if (selectedRoute){
+                if (selectedRoute == el.props.name){
+                    props.selected = true;
+                } else {
+                    props.selected = false;
+                }
+            }
+
+            var Icon = props.icon || console.error("No icon class is defined for "+el.name);
+            children.push(<Icon key={el.props.name} {...props}/>);
+            if (props.selected || index === 0){
+                selected = el;
+            }
+        });
+        return {children, hideTabBar: selected.props.hideTabBar};
+    }
+    componentWillMount(){
+        if (!this.props.children){
+            return;
+        }
+        this.state = this.getChildrenState(this.props.selected);
+
+    }
+
+    componentWillReceiveProps({selected}){
+        //console.log("TABBAR "+selected);
+        InteractionManager.runAfterInteractions(() =>
+            this.setState(this.getChildrenState(selected)));
+    }
+    render(){
+        if (this.state.hideTabBar){
+            return <View/>
+        }
+        return (
+            <Tabs style={{backgroundColor:'white'}} onSelect={this.onSelect.bind(this)} {...this.props}>
+                {this.state.children}
+            </Tabs>
+        );
+    }
+}
+
