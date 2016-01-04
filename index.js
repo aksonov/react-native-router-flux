2a6d1919053b2dc770de821e177b2674995c8dbd
Pavlo Aksonov <aksonov@MacBook-Pro-Pavlo.local>
2016-01-04 13:52:29 +0100

    fix animations

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import {Schema, Route} from './Common';
 import TabBar from './TabBar';
 import Actions from './Actions';
 import Router from './ReactRouter';
+import Animations from './Animations';
 
-module.exports = {Schema, Route, TabBar, Actions, Router};
+module.exports = {Schema, Route, TabBar, Actions, Router, Animations};
