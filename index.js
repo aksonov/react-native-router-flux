58baab69dfacf391639f254ae4398b3482978575
Daniel Weinmann <danielweinmann@gmail.com>
2016-02-10 20:21:38 -0200

    Renames Router to BaseRouter and ReactRouter to Router and router prop on Router to plugin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {Schema, Route} from './Common';
 import TabBar from './TabBar';
 import Actions from './Actions';
-import Router from './ReactRouter';
+import Router from './Router';
 import Animations from './Animations';
 
 module.exports = {Schema, Route, TabBar, Actions, Router, Animations};
