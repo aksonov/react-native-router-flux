3ed29a5e9f59de1188b565edecfaf2559380f315
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2016-04-24 17:20:50 -0400

    Fix some eslint errors

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+import Actions from './src/Actions';
+import DefaultRenderer from './src/DefaultRenderer';
+import Modal from './src/Modal';
+import NavBar from './src/NavBar';
+import Reducer from './src/Reducer';
 import Router from './src/Router';
 import Scene from './src/Scene';
-import Actions from './src/Actions';
-import getInitialState from './src/State';
-import Reducer from './src/Reducer';
-import TabBar from './src/TabBar';
-import Modal from './src/Modal';
-import DefaultRenderer from './src/DefaultRenderer';
 import Switch from './src/Switch';
-import NavBar from './src/NavBar';
+import TabBar from './src/TabBar';
+import getInitialState from './src/State';
 
-module.exports = { DefaultRenderer, NavBar, Switch, Modal, Router, TabBar,
-    Scene, Actions, Reducer, getInitialState };
+module.exports = {
+  Actions,
+  DefaultRenderer,
+  Modal,
+  NavBar,
+  Reducer,
+  Router,
+  Scene,
+  Switch,
+  TabBar,
+  getInitialState,
+};
--- src/Modal.js
+++ src/Modal.js
@@ -1,18 +1,22 @@
-import React, { Component, View } from 'react-native';
+import React, { View } from 'react-native';
 import DefaultRenderer from './DefaultRenderer';
 
-export default class extends Component {
-  render() {
-    const children = this.props.navigationState.children;
-    const state = children[0];
-    return (<View style={{ flex:1 }}>
-            <DefaultRenderer navigationState={state} key={state.key} {...state} />
-            {children.length > 1 && children.map((el, i) => {
-              if (i > 0 && el.component) {
-                const Component = el.component;
-                return <Component key={el.key} {...el} />;
-              }
-            })}
-        </View>);
-  }
+export default function Modal() {
+  const children = this.props.navigationState.children;
+  const state = children[0];
+  return (
+    <View style={{ flex: 1 }}>
+      <DefaultRenderer
+        navigationState={state}
+        key={state.key}
+        {...state}
+      />
+        {children.length > 1 && children.map((el, i) => {
+          if (i > 0 && el.component) {
+            const Component = el.component;
+            return <Component key={el.key} {...el} />;
+          }
+        })}
+    </View>
+  );
 }
