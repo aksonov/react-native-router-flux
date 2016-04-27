907688c3405de98320a9cab196c39ebda5b01615
Mike Fowler <mike.fowler@airbnb.com>
2016-04-27 13:50:53 -0400

    Fix eslint errors in DefaultRenderer.js

--- src/DefaultRenderer.js
+++ src/DefaultRenderer.js
@@ -6,28 +6,46 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-import React, { Component, Animated, PropTypes, StyleSheet, View, NavigationExperimental } from 'react-native';
-const {
-    AnimatedView: NavigationAnimatedView,
-    Card: NavigationCard,
-    RootContainer: NavigationRootContainer,
-    Header: NavigationHeader,
-    } = NavigationExperimental;
+import React, {
+  Animated,
+  Component,
+  NavigationExperimental,
+  PropTypes,
+  View,
+} from 'react-native';
+
 import TabBar from './TabBar';
 import NavBar from './NavBar';
 import Actions from './Actions';
 
+const {
+  AnimatedView: NavigationAnimatedView,
+  Card: NavigationCard,
+} = NavigationExperimental;
+
 export default class DefaultRenderer extends Component {
-  constructor(props) {
-    super(props);
-    this._renderCard = this._renderCard.bind(this);
-    this._renderScene = this._renderScene.bind(this);
-    this._renderHeader = this._renderHeader.bind(this);
-  }
+
+  static propTypes = {
+    navigationState: PropTypes.object,
+  };
 
   static childContextTypes = {
     navigationState: PropTypes.any,
   };
+
+  constructor(props) {
+    super(props);
+
+    this.renderCard = this.renderCard.bind(this);
+    this.renderScene = this.renderScene.bind(this);
+    this.renderHeader = this.renderHeader.bind(this);
+  }
+
+  getChildContext() {
+    return {
+      navigationState: this.props.navigationState,
+    };
+  }
 
   componentDidMount() {
     this.dispatchFocusAction(this.props);
@@ -47,62 +65,34 @@ export default class DefaultRenderer extends Component {
     Actions.focus({ scene });
   }
 
-  getChildContext() {
-    return {
-      navigationState: this.props.navigationState,
-    };
-  }
+  renderCard(/* NavigationSceneRendererProps*/ props) {
+    const { key, direction, panHandlers, getSceneStyle } = props.scene.navigationState;
 
-  render() {
-    const navigationState = this.props.navigationState;
-    if (!navigationState) {
-      return null;
-    }
-    let Component = navigationState.component;
-    if (navigationState.tabs && !Component) {
-      Component = TabBar;
-    }
-    if (Component) {
-      return (
-                <View style={[{ flex: 1 }, navigationState.sceneStyle]}>
-                    <Component {...navigationState} navigationState={navigationState} />
-                </View>
-            );
-    }
-
-    const selected = navigationState.children[navigationState.index];
-        // return <DefaultRenderer key={selected.key} navigationState={selected}/>
-
-    let applyAnimation = selected.applyAnimation || navigationState.applyAnimation;
-    let style = selected.style || navigationState.style;
-    let direction = selected.direction || navigationState.direction || 'horizontal';
-
-    let optionals = {};
-    if (applyAnimation) {
-      optionals.applyAnimation = applyAnimation;
-    } else {
-      let duration = selected.duration;
-      if (duration === null || duration === undefined) duration = navigationState.duration;
-      if (duration !== null && duration !== undefined) {
-        optionals.applyAnimation = function (pos, navState) {
-          Animated.timing(pos, { toValue: navState.index, duration }).start();
-        };
-      }
-    }
+    const optionals = {};
+    if (getSceneStyle) optionals.style = getSceneStyle(props);
 
     return (
-            <NavigationAnimatedView
-              navigationState={navigationState}
-              style={[styles.animatedView, style]}
-              renderOverlay={this._renderHeader}
-              direction={direction}
-              renderScene={this._renderCard}
-              {...optionals}
-            />
-        );
+      <NavigationCard
+        {...props}
+        key={`card_${key}`}
+        direction={direction || 'horizontal'}
+        panHandlers={panHandlers}
+        renderScene={this.renderScene}
+        {...optionals}
+      />
+    );
   }
 
-  _renderHeader(/* NavigationSceneRendererProps*/ props) {
+  renderScene(props: Object) {
+    return (
+      <DefaultRenderer
+        key={props.scene.navigationState.key}
+        navigationState={props.scene.navigationState}
+      />
+    );
+  }
+
+  renderHeader(props) {
     const state = props.navigationState;
     const child = state.children[state.index];
     let selected = state.children[state.index];
@@ -116,62 +106,95 @@ export default class DefaultRenderer extends Component {
     if (selected.component && selected.component.renderNavigationBar) {
       return selected.component.renderNavigationBar({ ...this.props, ...selected });
     }
-    const Component =  selected.navBar || child.navBar || state.navBar || NavBar;
-    //console.log("STATE:", state);
-    //console.log("CHILD:", child);
-    //console.log("SELECTED:", selected);
 
-    let navBarProps = {...state, ...child, ...selected};
-    // delete contrary properties
-    if ((selected.leftTitle || selected.leftButtonImage) && selected.onLeft){
+    const HeaderComponent = selected.navBar || child.navBar || state.navBar || NavBar;
+    const navBarProps = { ...state, ...child, ...selected };
+
+    if ((selected.leftTitle || selected.leftButtonImage) && selected.onLeft) {
       delete navBarProps.leftButton;
     }
-    if ((selected.rightTitle || selected.rightButtonImage) && selected.onRight){
+
+    if ((selected.rightTitle || selected.rightButtonImage) && selected.onRight) {
       delete navBarProps.rightButton;
     }
-    if (selected.rightButton){
+
+    if (selected.rightButton) {
       delete navBarProps.rightTitle;
       delete navBarProps.onRight;
       delete navBarProps.rightButtonImage;
     }
-    if (selected.leftButton){
+
+    if (selected.leftButton) {
       delete navBarProps.leftTitle;
       delete navBarProps.onLeft;
       delete navBarProps.leftButtonImage;
     }
     delete navBarProps.style;
 
-    const getTitle = selected.getTitle || ( state => state.title );
-    return <Component {...props} {...navBarProps} getTitle={getTitle} />;
+    const getTitle = selected.getTitle || (opts => opts.title);
+    return <HeaderComponent {...props} {...navBarProps} getTitle={getTitle} />;
   }
 
-  _renderCard(/* NavigationSceneRendererProps*/ props) {
-    const { key, direction, panHandlers, getSceneStyle } = props.scene.navigationState;
+  render() {
+    const navigationState = this.props.navigationState;
+
+    if (!navigationState) {
+      return null;
+    }
+
+    let SceneComponent = navigationState.component;
+
+    if (navigationState.tabs && !SceneComponent) {
+      SceneComponent = TabBar;
+    }
+
+    if (SceneComponent) {
+      return (
+        <View
+          style={[{ flex: 1 }, navigationState.sceneStyle]}
+        >
+          <SceneComponent
+            {...navigationState}
+            navigationState={navigationState}
+          />
+        </View>
+      );
+    }
 
     const optionals = {};
-    if (getSceneStyle) optionals.style = getSceneStyle(props);
+    const selected = navigationState.children[navigationState.index];
+    const applyAnimation = selected.applyAnimation || navigationState.applyAnimation;
+    const style = selected.style || navigationState.style;
+    let direction = selected.direction || navigationState.direction || 'horizontal';
+
+    if (applyAnimation) {
+      optionals.applyAnimation = applyAnimation;
+    } else {
+      let duration = selected.duration;
+      if (duration === null || duration === undefined) duration = navigationState.duration;
+      if (duration !== null && duration !== undefined) {
+        optionals.applyAnimation = (pos, navState) => {
+          Animated.timing(pos, { toValue: navState.index, duration }).start();
+        };
+      }
+    }
 
     return (
-            <NavigationCard
-              {...props}
-              key={'card_' + key}
-              direction={direction || 'horizontal'}
-              panHandlers={panHandlers}
-              renderScene={this._renderScene}
-              {...optionals}
-            />
-        );
-  }
-
-  _renderScene(/* NavigationSceneRendererProps*/ props) {
-    return <DefaultRenderer key={props.scene.navigationState.key} navigationState={props.scene.navigationState} />;
+      <NavigationAnimatedView
+        navigationState={navigationState}
+        style={[
+          {
+            flex: 1,
+            backgroundColor: 'transparent',
+          },
+          style,
+        ]}
+        renderOverlay={this.renderHeader}
+        direction={direction}
+        renderScene={this.renderCard}
+        {...optionals}
+      />
+    );
   }
 
 }
-
-const styles = StyleSheet.create({
-  animatedView: {
-    flex: 1,
-    backgroundColor:'transparent'
-  },
-});
