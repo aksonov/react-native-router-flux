9c0e6674be4c41b996fceecd5c96590588b95811
Mike Fowler <mike.fowler@airbnb.com>
2016-04-27 12:28:06 -0400

    Cleanup Modal.js

--- src/Modal.js
+++ src/Modal.js
@@ -1,18 +1,32 @@
-import React, { Component, View } from 'react-native';
+import React, {
+  PropTypes,
+  View,
+} from 'react-native';
 import DefaultRenderer from './DefaultRenderer';
 
-export default class extends Component {
-  render() {
-    const children = this.props.navigationState.children;
-    const state = children[0];
-    return (<View style={{ flex:1 }}>
-            <DefaultRenderer navigationState={state} key={state.key} {...state} />
-            {children.length > 1 && children.map((el, i) => {
-              if (i > 0 && el.component) {
-                const Component = el.component;
-                return <Component key={el.key} {...el} />;
-              }
-            })}
-        </View>);
-  }
+const propTypes = {
+  navigationState: PropTypes.shape({
+    children: PropTypes.array,
+  }),
+};
+
+export default function Modal(props: Object) {
+  const children = props.navigationState.children;
+  const state = children[0];
+
+  return (
+    <View style={{ flex: 1 }}>
+      <DefaultRenderer navigationState={state} key={state.key} {...state} />
+      {children.length > 1 && children.map((el, i) => {
+        if (i > 0 && el.component) {
+          const Component = el.component;
+          return <Component key={el.key} {...el} />;
+        }
+
+        return null;
+      })}
+    </View>
+  );
 }
+
+Modal.propTypes = propTypes;
