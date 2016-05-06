cebe060e7518affe55ac8d282930ae6201966cf9
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2016-05-05 20:15:48 -0400

    Fix eslint Example/components/Register.js

--- Example/components/Register.js
+++ Example/components/Register.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet} from "react-native";
-import Button from "react-native-button";
-import {Actions} from "react-native-router-flux";
+import { View, Text, StyleSheet } from 'react-native';
+import Button from 'react-native-button';
+import { Actions } from 'react-native-router-flux';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF",
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: "center",
-        margin: 10,
-    },
-    instructions: {
-        textAlign: "center",
-        color: "#333333",
-        marginBottom: 5,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
 });
 
-export default class extends React.Component {
-    render(){
-        return (
-            <View style={styles.container}>
-                <Text>Register page</Text>
-                <Button onPress={Actions.home}>Replace screen</Button>
-                <Button onPress={Actions.pop}>Back</Button>
-            </View>
-        );
-    }
-}
+const Register = () => (
+  <View style={styles.container}>
+    <Text>Register page</Text>
+    <Button onPress={Actions.home}>Replace screen</Button>
+    <Button onPress={Actions.pop}>Back</Button>
+  </View>
+);
+
+export default Register;
