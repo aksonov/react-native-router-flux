486e03a642a1cba80b8beab79725783eb5d43bb4
Joe Noon <joenoon@gmail.com>
2016-05-12 14:57:52 -0700

    use hideNavBar passed-in prop to determine marginTop

--- Example/components/EchoView.js
+++ Example/components/EchoView.js
@@ -17,16 +17,49 @@ const styles = StyleSheet.create({
         color: "#333333",
         marginBottom: 5,
     },
+    smaller: {
+        marginBottom: 5,
+        fontSize: 12,
+    },
 });
 
 
 export default class extends React.Component {
     render(){
         return (
-            <View style={[styles.container, this.props.sceneStyle]}>
+            <View style={[styles.container, this.props.sceneStyle, !this.props.hideNavBar ? { marginTop: 64 } : null]}>
                 <Text style={styles.instructions}>key: {this.props.navigationState.key}</Text>
                 <Text style={styles.instructions}>sceneKey: {this.props.navigationState.sceneKey}</Text>
-                <Button onPress={Actions.echo}>push new scene</Button>
+                <Button
+                  onPress={() => Actions.echo()}
+                  style={styles.smaller}
+                >
+                  push new scene hideNavBar=inherit hideTabBar=inherit
+                </Button>
+                <Button
+                  onPress={() => Actions.echo({ hideNavBar: true, hideTabBar: true })}
+                  style={styles.smaller}
+                >
+                  push new scene hideNavBar=true hideTabBar=true
+                </Button>
+                <Button
+                  onPress={() => Actions.echo({ hideNavBar: true, hideTabBar: false })}
+                  style={styles.smaller}
+                >
+                  push new scene hideNavBar=true hideTabBar=false
+                </Button>
+                <Button
+                  onPress={() => Actions.echo({ hideNavBar: false, hideTabBar: true })}
+                  style={styles.smaller}
+                >
+                  push new scene hideNavBar=false hideTabBar=true
+                </Button>
+                <Button
+                  onPress={() => Actions.echo({ hideNavBar: false, hideTabBar: false })}
+                  style={styles.smaller}
+                >
+                  push new scene hideNavBar=false hideTabBar=false
+                </Button>
                 <Button onPress={Actions.pop}>pop</Button>
             </View>
         );
--- Example/components/TabView.js
+++ Example/components/TabView.js
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
 const TabView = (props, context) => {
   const drawer = context.drawer;
   return (
-    <View style={[styles.container, props.sceneStyle]}>
+    <View style={[styles.container, props.sceneStyle, !props.hideNavBar ? { marginTop: 64 } : null ]}>
       <Text>Tab {props.title}</Text>
       {props.name === 'tab1_1' &&
         <Button onPress={Actions.tab1_2}>next screen for tab1_1</Button>
