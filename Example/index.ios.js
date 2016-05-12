7cee8d2e6c42ca4efd20435e817c159906293865
Joe Noon <joenoon@gmail.com>
2016-05-12 12:16:00 -0700

    temporarily ignore upstream RN bug

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -1,5 +1,8 @@
 import { AppRegistry } from 'react-native';
 
+// @todo remove when RN upstream is fixed
+console.ignoredYellowBox = ['Warning: Failed propType: SceneView'];
+
 import Example from './Example';
 
 AppRegistry.registerComponent('Example', () => Example);
