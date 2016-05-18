fc843019da727a007e6228202a3af2294c29e4a1
Andre Nanninga <ag.nanninga@gmail.com>
2016-05-18 10:18:28 +0200

    removed 'Integer.isNumber' and added falsy check for 'titleNumerOfLines'

--- src/NavBar.js
+++ src/NavBar.js
@@ -325,9 +325,7 @@ class NavBar extends React.Component {
 
   renderTitle(childState, index:number) {
     const title = this.props.getTitle ? this.props.getTitle(childState) : childState.title;
-
-    let numberOfLines = this.props.titleNumberOfLines;
-    numberOfLines = Number.isInteger(numberOfLines) ? numberOfLines : -1;
+    const numberOfLines = this.props.titleNumberOfLines || -1;
 
     return (
       <Animated.Text
