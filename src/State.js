38362999840c9ad19fc9da2adab73da0ee8a4412
Joe Noon <joenoon@gmail.com>
2016-05-18 23:43:20 -0700

    remove dependency on assert (#698)

--- src/State.js
+++ src/State.js
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-import assert from 'assert';
+import { assert } from './Util';
 
 function getStateFromScenes(route, scenes, props) {
   const getters = [];
