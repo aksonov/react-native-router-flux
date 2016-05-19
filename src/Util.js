236e449f8b65674f5b2bb54cb7718b659f9c5929
Joe Noon <joenoon@gmail.com>
2016-05-18 20:44:32 -0700

    change export/import style for Util

--- src/Util.js
+++ src/Util.js
@@ -1,3 +1,34 @@
+// searches for the deepest explicitly set value for a key
+// in a navigationState tree.
+export function deepestExplicitValueForKey(navigationState, key) {
+  let current;
+  let selected = navigationState;
+
+  while (selected.hasOwnProperty('children')) {
+    if (!selected.tabs) {
+      // for pushed children, iterate through each, recording key value,
+      // until reaching the selected child
+      for (let i = 0; i < selected.index; i++) {
+        if (typeof(selected.children[i][key]) !== 'undefined') {
+          current = selected.children[i][key];
+        }
+      }
+    }
+    // set the new selected child and check for a key value
+    selected = selected.children[selected.index];
+    if (typeof(selected[key]) !== 'undefined') {
+      current = selected[key];
+    }
+  }
+
+  // fallback to the root key value
+  if (typeof(current) === 'undefined') {
+    current = navigationState[key];
+  }
+
+  return current;
+}
+
 export function assert(expr, failDescription) {
   if (!expr) {
     throw new Error(`[react-native-router-flux] ${failDescription}`);
@@ -5,41 +36,6 @@ export function assert(expr, failDescription) {
 }
 
 export default {
-
-  // searches for the deepest explicitly set value for a key
-  // in a navigationState tree.
-  deepestExplicitValueForKey(navigationState, key) {
-    let current;
-    let selected = navigationState;
-
-    while (selected.hasOwnProperty('children')) {
-      if (!selected.tabs) {
-        // for pushed children, iterate through each, recording key value,
-        // until reaching the selected child
-        for (let i = 0; i < selected.index; i++) {
-          if (typeof(selected.children[i][key]) !== 'undefined') {
-            current = selected.children[i][key];
-          }
-        }
-      }
-      // set the new selected child and check for a key value
-      selected = selected.children[selected.index];
-      if (typeof(selected[key]) !== 'undefined') {
-        current = selected[key];
-      }
-    }
-
-    // fallback to the root key value
-    if (typeof(current) === 'undefined') {
-      current = navigationState[key];
-    }
-
-    return current;
-  },
+  deepestExplicitValueForKey,
+  assert,
 };
-
-export function assert(expr, failDescription) {
-  if (!expr) {
-    throw new Error(`[react-native-router-flux] ${failDescription}`);
-  }
-}
