0108dbd86ae6ec693160fb40102aafa523af85d0
Pavlo Aksonov <aksonov@gmail.com>
2016-05-25 17:27:01 +0200

    make Switch as pure function

--- src/Switch.js
+++ src/Switch.js
@@ -1,59 +1,20 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 import DefaultRenderer from './DefaultRenderer';
-import Actions from './Actions';
 
-export default class extends Component {
-
-  static propTypes = {
-    onNavigate: PropTypes.func,
-  };
-
-  constructor(props) {
-    super(props);
-    this.updateState = this.updateState.bind(this);
-    this.state = {};
-  }
-
-  componentDidMount() {
-    this.updateState(this.props);
-  }
-
-  componentWillReceiveProps(props) {
-    this.updateState(props);
-  }
-
-  updateState(props) {
-    const navState = props.navigationState;
-
-    const selector = props.selector;
-    if (!selector) console.error('Selector should be defined.');
-
-    const selectedKey = selector(props);
-    if (!selectedKey) console.error('Selector should return key.');
-
-    const selected = navState.children.filter(el => el.sceneKey === selectedKey);
-    if (!selected) console.error(`A scene for key “${selectedKey}” does not exist.`);
-
-    const navigationState = selected[0];
-    if (!navigationState) console.error(`Cannot find a scene with key “${selectedKey}”`);
-
-    if (navigationState.key !== navState.children[navState.index].key) {
-      Actions[selectedKey]();
-    }
-
-    this.setState({ navigationState });
-  }
-
-  render() {
-    if (this.state.navigationState) {
-      return (
-        <DefaultRenderer
-          onNavigate={this.props.onNavigate}
-          navigationState={this.state.navigationState}
-        />
-      );
-    }
-
-    return null;
-  }
+export default function Switch(props) {
+  const navState = props.navigationState;
+  const selector = props.selector;
+  if (!selector) console.error('Selector should be defined.');
+  const selectedKey = selector(props);
+  if (!selectedKey) console.error('Selector should return key.');
+  const selected = navState.children.filter(el => el.sceneKey === selectedKey);
+  if (!selected) console.error(`A scene for key “${selectedKey}” does not exist.`);
+  const navigationState = selected[0];
+  if (!navigationState) console.error(`Cannot find a scene with key “${selectedKey}”`);
+  return (
+    <DefaultRenderer
+      onNavigate={this.props.onNavigate}
+      navigationState={navigationState}
+    />
+  );
 }
