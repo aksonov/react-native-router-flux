acc440abb1593840f0d68eddd8688f8f1d4a9c59
Code <mycalf@me.com>
2016-06-07 10:56:10 +0800

    tabs show onPress item
    <Scene key="Member"  tabs={true} >
                <Scene key="Launch"  onPress={()=>Actions._Launch()} />
    </Scene>

--- src/Actions.js
+++ src/Actions.js
@@ -80,7 +80,7 @@ class Actions {
     if (!(list instanceof Array)) {
       list = [list];
     }
-    const condition = el => (!el.props.component && !el.props.children &&
+    const condition = el => (!el.props.component && !el.props.children && !el.props.onPress &&
     (!el.props.type || el.props.type === REFRESH_ACTION));
     // determine sub-states
     let baseKey = root.key;
@@ -90,7 +90,9 @@ class Actions {
     if (list.length) {
       res.children = list.map(c => this.iterate(c, res, refs).key);
     } else {
-      assert(staticProps.component, `component property is not set for key=${key}`);
+      if (!staticProps.onPress){
+          assert(staticProps.component, `component property is not set for key=${key}`);
+      }
       // wrap scene if parent is "tabs"
       if (parentProps.tabs) {
         const innerKey = `${res.key}_`;
