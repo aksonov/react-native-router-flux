c673ed012c9c23608539f86c7367dadff7a9c57f
Rafael Seemann <rseemann@gmail.com>
2016-06-12 20:53:35 +0200

    Add Login 3 component with different PopTo examples

---
+++ Example/components/Login3.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, StyleSheet} from "react-native";
+import Button from "react-native-button";
+import {Actions} from "react-native-router-flux";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+    },
+});
+
+const popToRoot = () => {
+  Actions.popTo("root");
+}
+
+const popToLogin1 = () => {
+  Actions.popTo("loginModal");
+}
+
+const popToLogin2 = () => {
+  Actions.popTo("loginModal2");
+}
+
+export default class extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Login2 page: {this.props.data}</Text>
+                <Button onPress={Actions.pop}>Back</Button>
+                <Button onPress={popToLogin1}>To Login</Button>
+                <Button onPress={popToLogin2}>To Login2</Button>
+                <Button onPress={popToRoot}>To Root</Button>
+            </View>
+        );
+    }
+}
