a769b10288cab94f5213d243b61744c2cdf801cf
Rowell <rowellx68@users.noreply.github.com>
2016-06-22 15:58:48 +0100

    Render leftButtonImage (#838)
    * Fix for leftButtonIcon not rendering
    
    * check for buttonImage instead of menuIcon
    
    * tidied up the setting of Image

--- src/NavBar.js
+++ src/NavBar.js
@@ -373,7 +373,11 @@ class NavBar extends React.Component {
             }
             {buttonImage &&
               <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
-                {menuIcon}
+                {menuIcon || <Image
+                  source={buttonImage}
+                  style={state.leftButtonIconStyle || styles.defaultImageStyle}
+                />
+                }
               </View>
             }
           </TouchableOpacity>
