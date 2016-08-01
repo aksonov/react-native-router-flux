b38aeb7145c7e8ce9ed26eef5774654333111399
Joe Noon <joenoon@gmail.com>
2016-08-01 01:31:41 -0700

    revert to old Switch-style, keeping unmountScenes.  needs review

--- src/Switch.js
+++ src/Switch.js
@@ -1,59 +1,71 @@
-import React, { PropTypes } from 'react';
-import TabBar from './TabBar';
-import { assert } from './Util';
+import React, { Component, PropTypes } from 'react';
 
-export default function Switch(props) {
-  const navState = props.navigationState;
-  const selector = props.selector;
-  const statem = props.statem;
-  if (!selector && !statem) console.error('Selector should be defined.');
-  let index = -1;
-  if (!selector) {
-    // support Statem - Harel statecharts machine!
-    navState.children.forEach((el, i) => {
-      assert(el.default || el.state,
-        `Either default or state should be defined for element=${el.key}`);
-      if (el.default) {
-        index = i;
-      } else {
-        if (el.state.active) {
-          index = i;
-        }
-      }
-    });
-    assert(index !== -1, 'No default scene is defined');
-  } else {
+import DefaultRenderer from './DefaultRenderer';
+import Actions from './Actions';
+
+export default class Switch extends Component {
+
+  static propTypes = {
+    navigationState: PropTypes.object,
+    onNavigate: PropTypes.func,
+    selector: PropTypes.func,
+    unmountScenes: PropTypes.bool,
+  };
+
+  constructor(props, context) {
+    super(props, context);
+    this.updateState = this.updateState.bind(this);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.updateState(this.props);
+  }
+
+  componentWillReceiveProps(props) {
+    this.updateState(props);
+  }
+
+  updateState(props) {
+    const navState = props.navigationState;
+
+    const selector = props.selector;
+    if (!selector) console.error('Selector should be defined.');
+
     const selectedKey = selector(props);
     if (!selectedKey) console.error('Selector should return key.');
-    navState.children.forEach((el, i) => {
-      if (el.sceneKey === selectedKey) {
-        index = i;
-      }
-    });
-    if (index === -1) console.error(`A scene for key “${selectedKey}” does not exist.`);
-  }
-  let navigationState = index !== navState.index ? { ...navState, index } : navState;
 
-  if (props.unmountScenes) {
-    navigationState = {
-      ...navigationState,
-      children: [navState.children[navigationState.index]],
-      index: 0,
-    };
+    const selected = navState.children.filter(el => el.sceneKey === selectedKey);
+    if (!selected) console.error(`A scene for key “${selectedKey}” does not exist.`);
+
+    let navigationState = selected[0];
+    if (!navigationState) console.error(`Cannot find a scene with key “${selectedKey}”`);
+
+    if (navigationState.key !== navState.children[navState.index].key) {
+      Actions[selectedKey]();
+    }
+
+    if (props.unmountScenes) {
+      navigationState = {
+        ...navigationState,
+        children: [navState.children[navigationState.index]],
+        index: 0,
+      };
+    }
+
+    this.setState({ navigationState });
   }
-  return (
-    <TabBar
-      onNavigate={props.onNavigate}
-      navigationState={navigationState}
-      unmountScenes={props.unmountScenes}
-    />
-  );
+
+  render() {
+    if (this.state.navigationState) {
+      return (
+        <DefaultRenderer
+          onNavigate={this.props.onNavigate}
+          navigationState={this.state.navigationState}
+        />
+      );
+    }
+
+    return null;
+  }
 }
-
-Switch.propTypes = {
-  navigationState: PropTypes.object,
-  onNavigate: PropTypes.func,
-  selector: PropTypes.func,
-  statem: PropTypes.any,
-  unmountScenes: PropTypes.bool,
-};
