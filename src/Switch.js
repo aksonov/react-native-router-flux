45762f1b5d2045dd2aa05f60c3527aa91724d1bf
Pavel Aksonov <aksonov@users.noreply.github.com>
2016-08-04 10:09:54 +0200

    Restore statem (#1013)
    * restore statem selector logic
    
    * remove import
    
    * fix ESLint errors

--- src/Switch.js
+++ src/Switch.js
@@ -6,21 +6,35 @@ export default function Switch(props) {
   const navState = props.navigationState;
 
   const selector = props.selector;
-  if (!selector) console.error('Selector should be defined.');
-
-  const selectedKey = selector(props);
-  if (!selectedKey) console.error('Selector should return key.');
-
-  const selected = navState.children.filter(el => el.sceneKey === selectedKey);
-  if (!selected) console.error(`A scene for key “${selectedKey}” does not exist.`);
-
+  const statem = props.statem;
+  if (!selector && !statem) console.error('Selector should be defined.');
   let index = -1;
-  navState.children.forEach((el, i) => {
-    if (el.sceneKey === selectedKey) {
-      index = i;
-    }
-  });
+  let selectedKey = undefined;
+  if (!selector) {
+    // support Statem - Harel statecharts machine!
+    navState.children.forEach((el, i) => {
+      if (!(el.default || el.state)) {
+        console.error(`Either default or state should be defined for element=${el.key}`);
+      }
+      if (el.default) {
+        index = i;
+      } else {
+        if (el.state.active) {
+          index = i;
+        }
+      }
+    });
+  } else {
+    selectedKey = selector(props);
+    if (!selectedKey) console.error('Selector should return key.');
+    navState.children.forEach((el, i) => {
+      if (el.sceneKey === selectedKey) {
+        index = i;
+      }
+    });
+  }
   if (index === -1) console.error(`A scene for key “${selectedKey}” does not exist.`);
+  selectedKey = navState.children[index].sceneKey;
 
   let navigationState;
   if (index !== navState.index) {
