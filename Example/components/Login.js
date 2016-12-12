ad8698d81ca8063f3afe3e7c106f0ce51ab8f8f8
Mingyu Kim <mechanic0406@gmail.com>
2016-12-12 15:27:17 +0900

    Fixed ‘Example’ project source
    If no data is delivered from previous page(Login.js),
    ‘this.props.data’ has previous properties on prototype.
    So, it has no problem in ‘Release’ mode(iOS).
    But, ‘Debug’ mode is not.(You can see a red screen)
    
    I have tested the project on master branch and I got a same experience.
    So, I fixed some sources.

--- Example/components/Login.js
+++ Example/components/Login.js
@@ -25,10 +25,14 @@ const styles = StyleSheet.create({
 
 export default class extends React.Component {
     render(){
+        const title = this.props.title || 'No Title';
+        const data = this.props.data || 'No Data';
         return (
             <View style={[styles.container, this.props.style]}>
-                <Text>Login page: {this.props.data}</Text>
-                <Button onPress={Actions.loginModal2}>Login 2</Button>
+                <Text>Login page 1</Text>
+                <Text>Title: {title}</Text>
+                <Text>Data: {data}</Text>
+                <Button onPress={() => Actions.loginModal2({ data:"Custom data2", title:"Custom title2" })}>Login 2</Button>
                 <Button onPress={() => Actions.refresh({title:"Changed title"})}>Change title</Button>
                 <Button onPress={Actions.pop}>Back</Button>
             </View>
