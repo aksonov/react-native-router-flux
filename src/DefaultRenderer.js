2b7fcd32945ca9935f12f365b5f59af2af96e40b
ahanriat <ahanriat@gmail.com>
2017-02-13 10:01:42 +0100

    feature: expose position props to allow custom animations

--- src/DefaultRenderer.js
+++ src/DefaultRenderer.js
@@ -141,6 +141,7 @@ export default class DefaultRenderer extends Component {
     return (
       <DefaultRenderer
         key={props.scene.navigationState.key}
+        position={props.position}
         onNavigate={props.onNavigate}
         navigationState={props.scene.navigationState}
       />
