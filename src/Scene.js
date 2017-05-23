ed78249840756f4d35e344da67dfcd8e0957f343
Jelle Breuer <jelle@breuclan.nl>
2017-05-23 15:31:47 +0200

    Updated View.propTypes to ViewPropTypes

--- src/Scene.js
+++ src/Scene.js
@@ -7,19 +7,19 @@
  *
  */
 import React, { PropTypes } from 'react';
-import { View, Text } from 'react-native';
+import { ViewPropTypes, Text } from 'react-native';
 
 export default class extends React.Component {
 
   // @todo - should all props be documented/specified here?
 
   static propTypes = {
-    tabBarStyle: View.propTypes.style,
-    tabBarSelectedItemStyle: View.propTypes.style,
-    tabBarIconContainerStyle: View.propTypes.style,
-    tabBarShadowStyle: View.propTypes.style,
-    tabSceneStyle: View.propTypes.style,
-    tabStyle: View.propTypes.style,
+    tabBarStyle: ViewPropTypes.style,
+    tabBarSelectedItemStyle: ViewPropTypes.style,
+    tabBarIconContainerStyle: ViewPropTypes.style,
+    tabBarShadowStyle: ViewPropTypes.style,
+    tabSceneStyle: ViewPropTypes.style,
+    tabStyle: ViewPropTypes.style,
     tabTitleStyle: Text.propTypes.style,
     tabSelectedTitleStyle: Text.propTypes.style,
     tabTitle: PropTypes.string,
