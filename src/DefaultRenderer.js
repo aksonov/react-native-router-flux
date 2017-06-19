760169068baa4bc198f4b43df83604440af31b93
Faruk Can Bilir <farkob@gmail.com>
2017-06-19 17:03:40 +0300

    Update DefaultRenderer.js

--- src/DefaultRenderer.js
+++ src/DefaultRenderer.js
@@ -314,7 +314,7 @@ export default class DefaultRenderer extends PureComponent {
           if (duration === 0) {
             pos.setValue(navState.index);
           } else {
-            Animated.timing(pos, { toValue: navState.index, duration }).start();
+            Animated.timing(pos, { toValue: navState.index, duration, useNativeDriver: true }).start();
           }
         };
       }
