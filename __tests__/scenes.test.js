71a1309d6ecc4e68b950a921d129c58483c7a34b
aksonov <aksonov@gmail.com>
2017-06-23 13:10:57 +0200

    fix unit tests

--- __tests__/scenes.test.js
+++ __tests__/scenes.test.js
@@ -5,6 +5,9 @@ import {when} from 'mobx';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import Router from '../src/Router';
+import Scene from '../src/Scene';
+import navigationStore from '../src/navigationStore';
+
 class B extends React.Component {
   render(){
     return <Text>Hello world!</Text>;
@@ -12,48 +15,22 @@ class B extends React.Component {
 }
 
 
-const scenes = new Router({
-  a: {
-    component: B,
-    props: {},
-    login: async(username, password) => await loginStore.login(username, password) ? scenes.c() : scenes.refresh({error: 'wrong password'})
-  },
-  b: {
-    initial: true,
-    children: () => [scenes.a, scenes.c],
-    props: {},
-  },
-  c: {
-    component: B,
-  }
-});
-
-class BB {
-  get a() {
-    return 1;
-  }
-
-  a(){
-    return "123"
-  }
-}
-
+const router = Router(<Scene>
+  <Scene key="b">
+    <Scene key="a" component={B} param="1"/>
+    <Scene key="c" component={B} param="2"/>
+  </Scene>
+</Scene>);
 
 test('renders correctly', done => {
-  renderer.create(scenes.create());
-  when(()=>scenes.screen, ()=>{
-    try {
-      console.log("STATE:", JSON.stringify(scenes.state));
-      scenes.c({data: 'abc'});
-      scenes.refresh({data: 'abcde'});
-      console.log("STATE:", JSON.stringify(scenes.state));
-      scenes.pop();
-      console.log("STATE:", JSON.stringify(scenes.state));
-      scenes.refresh({a: 3});
-      console.log("STATE:", JSON.stringify(scenes.state));
-      done();
-    } catch (e){
-      done(e);
-    }
-  })
+  renderer.create(router);
+  console.log("STATE:", JSON.stringify(navigationStore.state));
+  navigationStore.c({data: 'abc'});
+  navigationStore.refresh({data: 'abcde'});
+  console.log("STATE:", JSON.stringify(navigationStore.state));
+  navigationStore.pop();
+  console.log("STATE:", JSON.stringify(navigationStore.state));
+  navigationStore.refresh({a: 3});
+  console.log("STATE:", JSON.stringify(navigationStore.state));
+  done();
 });
