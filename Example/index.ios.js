d962c468a4b15ea3b4810c1d410cdc151d797485
aksonov <aksonov@gmail.com>
2017-06-23 12:52:08 +0200

    fix demo

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -1,8 +1,30 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import {StackNavigator, TabNavigator} from 'react-navigation';
+import {Router, Scene} from 'react-native-router-flux';
 
-// @todo remove when RN upstream is fixed
-console.ignoredYellowBox = ['Warning: Failed propType: SceneView'];
+import Launch from './components/Launch';
+import Login from './components/Login';
+import Login2 from './components/Login2';
+import TabView from './components/TabView';
 
-import Example from './Example';
+// const SimpleApp = TabNavigator({
+//   Home: { screen: Launch },
+//   TabView: {screen: TabView}
+//
+// },{
+//   navigationOptions: {header: null, tabBarVisible: false}
+//
+// });
+//
 
-AppRegistry.registerComponent('Example', () => Example);
+const App = Router(<Scene hideTabBar tabs>
+  <Scene key="launch" component={Launch} />
+  <Scene key="loginContainer" modal>
+    <Scene key="login" component={Login} title="Login!!!"/>
+    <Scene key="login2" component={Login} title="Login2"/>
+  </Scene>
+</Scene>);
+
+
+AppRegistry.registerComponent('Example', () => App);
--- Example/jest/setup.js
+++
@@ -1,9 +0,0 @@
-jest.mock('Linking', () => {
-  return {
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    openURL: jest.fn(),
-    canOpenURL: jest.fn(),
-    getInitialURL: jest.fn(),
-  }
-})
