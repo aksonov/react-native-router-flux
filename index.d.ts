0a3ea2b2caa0a049bc4e1343b3e3ccb0ed92b654
Erk <erk.pieper@gmail.com>
2017-11-29 11:48:55 +0100

    Quick fixes for typing errors on: error TS2411

--- index.d.ts
+++ index.d.ts
@@ -148,8 +148,8 @@ interface ActionsStatic {
     refresh: (props?: any) => void;
     replace: (sceneKey: string, props?: any) => void;
     reset: (sceneKey: string, props?: any) => void;
-    drawerOpen?: () => void;
-    drawerClose?: () => void;
+    drawerOpen?: any;
+    drawerClose?: any;
 
 }
 interface ActionsGenericStatic extends ActionsStatic {
