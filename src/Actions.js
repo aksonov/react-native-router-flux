627d9bd0a67e70bd01f744d377fefa2f45b874b7
David Pineda <davidpineda1003@gmail.com>
2018-02-01 15:37:41 -0500

    Quit libs

--- src/Actions.js
+++ src/Actions.js
@@ -1,12 +1,12 @@
-// /**
-//  * Copyright (c) 2015-present, Pavel Aksonov
-//  * All rights reserved.
-//  *
-//  * This source code is licensed under the BSD-style license found in the
-//  * LICENSE file in the root directory of this source tree.
-//  *
-//  */
-// import { assert } from './Util';
+/**
+ * Copyright (c) 2015-present, Pavel Aksonov
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { assert } from './Util';
 // import Scene from './Scene';
 // import * as ActionConst from './ActionConst';
 
