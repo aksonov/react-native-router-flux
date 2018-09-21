1d03201b6ab0ebff7dcac8cd609011ddd840bee3
Davis Z. Cabral <daviscabral@gmail.com>
2018-09-21 08:36:46 -0300

    rnrf-cli: initial work for a CLI (#3286)

---
+++ packages/react-native-router-flux-cli/README.md
@@ -0,0 +1,1 @@
+# React Native Router Flux CLI tools
---
+++ packages/react-native-router-flux-cli/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const rimraf = require('rimraf');
+
+const [,, command, ...args] = process.argv;
+
+const commands = {
+  dedup: function () {
+    const blacklisted = [
+      'node_modules/react-native-router-flux/node_modules',
+      'node_modules/react-native-router-flux/Example',
+      'node_modules/react-native-router-flux/ReduxExample',
+      'node_modules/react-native-router-flux/ExpoExample'
+    ];
+
+    blacklisted.map(function (part) {
+      const current = path.join(process.env.PWD, part);
+      rimraf(current, function (err) {
+        if (err) throw err
+        console.log(`${current} removed!`);
+      });
+    });
+  }
+};
+
+commands[command](...args);
