3871a06ce817deb76079a25d028a9ae8b1d980ca
HYUNGU, KANG <gusrn1423@naver.com>
2019-09-04 19:32:35 +0900

    [revert] remove Statusbar marginTop for navTransparent (#3493)
    * navigationStore : removed margin top when navTransparent
    
    * added empty header Style
    
    * fixing eslint

--- src/navigationStore.js
+++ src/navigationStore.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  StatusBar, Image, Animated, Easing,
-} from 'react-native';
+import { Image, Animated, Easing } from 'react-native';
 import {
   createAppContainer,
   createBottomTabNavigator,
@@ -346,7 +344,7 @@ function createNavigationOptions(params) {
 
     if (navTransparent) {
       res.headerTransparent = true;
-      res.headerStyle = { marginTop: StatusBar.currentHeight };
+      res.headerStyle = {};
     }
 
     if (!legacy && backToInitial) {
