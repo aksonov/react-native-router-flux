c8bf23223415fdabaeca525087aabd19ddbd299c
Frieder Bluemle <frieder.bluemle@gmail.com>
2020-01-14 11:29:36 -0800

    Update react-native to 0.61.5

--- examples/react-native/android/app/src/main/java/com/example/MainActivity.java
+++ examples/react-native/android/app/src/main/java/com/example/MainActivity.java
@@ -5,8 +5,8 @@ import com.facebook.react.ReactActivity;
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
