e4f61e264b7c84fc0083aff520fc9a2a26e6b411
Reflect Code <reflectcodelabs@gmail.com>
2022-10-29 21:19:33 +0530

    Resolved ViewPropTypes removed from React Native
    Resolved ViewPropTypes removed from React Native
    
    https://github.com/aksonov/react-native-router-flux/issues/3826

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { ViewPropTypes, BackHandler, Linking } from 'react-native';
+
+import { BackHandler, Linking } from 'react-native';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
+
 import PropTypes from 'prop-types';
 import NavigationStore from './Store';
 import defaultStore from './defaultStore';
